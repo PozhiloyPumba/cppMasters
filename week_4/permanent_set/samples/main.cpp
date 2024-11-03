fcf633ab3a1b1462caeed2cad9faab94835fa9b3
PozhiloyPumba <ivanov.in@phystech.edu>
2024-11-03 03:31:08 +0300

    added e2e ability

--- week_4/permanent_set/samples/main.cpp
+++ week_4/permanent_set/samples/main.cpp
@@ -2,45 +2,62 @@
 #include <iostream>
 #include <fstream>
 
-int main() {
-    std::ofstream file;
-    file.open("test.dot");
-    persistent::Set<std::string> t2;
-    t2.insert("abc");
-    t2.undo();
-    t2.insert("ab");
-    t2.insert("abd");
-    t2.insert("aaa");
-    t2.undo();
-    t2.insert("bim");
-    t2.insert("bam");
-    t2.insert("bimbim");
-    t2.insert("bambam");
-    t2.undo();
-    // t2.insert("b");
-    // t2.remove("bim");
-    // t2.undo();
+enum class string_code {
+    UNDO,
+    REDO,
+    INSERT,
+    DELETE,
+    CONTAINS,
 
-    // t2.dumpTree(file);
-    std::cout << t2.contains("bim") << std::endl;
-    file.close();
-    
-    persistent::Set<std::wstring> t3;
-    std::wofstream f("test.txt");
-    t3.dumpTree(f);
+    INVALID
+};
+
+using enum string_code;
+
+static string_code hashit (const std::string & inString) {
+    if (inString == "u") return UNDO;
+    if (inString == "r") return REDO;
+    if (inString == "i") return INSERT;
+    if (inString == "d") return DELETE;
+    if (inString == "c") return CONTAINS;
+    return INVALID;
+}
+int main() {
     persistent::Set<int> t;
-    t.insert(5);
-    // t.undo();
-    // std::cout << t.contains(5) << std::endl;
-    // t.insert(3);
-    // t.insert(2);
-    // std::cout << t.contains(2) << std::endl;
-    // t.redo();
-    // std::cout << t.contains(2) << std::endl;
-    // t.insert(4);
-    // t.undo();
-    // t.insert(9);
-    // t.insert(7);
-    // std::cout << t.contains(4) << std::endl;
+
+    std::string opt;
+    std::cin >> opt;
+    while(opt != "e") {
+        switch(hashit(opt)) {
+            case UNDO: {
+                t.undo();
+                break;
+            }
+            case REDO: {
+                t.redo();
+                break;
+            }
+            case INSERT: {
+                int tmp;
+                std::cin >> tmp;
+                t.insert(tmp);
+                break;
+            }
+            case DELETE: {
+                int tmp;
+                std::cin >> tmp;
+                t.remove(tmp);
+                break;
+            }
+            case CONTAINS: {
+                int tmp;
+                std::cin >> tmp;
+                std::cout << t.contains(tmp) << std::endl;
+                break;
+            }
+            default:;
+        }
+        std::cin >> opt;
+    }
     return 0;
 }
