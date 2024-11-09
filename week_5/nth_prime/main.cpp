66debb0d1c13b2d173219a0f07b7d8e54882fb50
PozhiloyPumba <ivanov.in@phystech.edu>
2024-11-10 00:31:02 +0300

    final version

--- week_5/nth_prime/main.cpp
+++ week_5/nth_prime/main.cpp
@@ -24,30 +24,39 @@ template<int n>
 struct is_prime : impl::is_prime<n, Sqrt<n>::value>{};
 
 namespace impl {
-    template<unsigned n, int shift = 2>
-    struct Nth_prime : 
+    template<unsigned n>
+    struct Nth_prime;
+	
+	template<unsigned n>
+    struct NextShift : std::integral_constant<unsigned, (n % 6 + 3) % 6>{};
+
+    template<unsigned n, int shift>
+    struct Nth_prime_shifted : 
         std::integral_constant<unsigned, 
             std::conditional_t<
-                ::is_prime<impl::Nth_prime<n - 1>::value + shift>::value,
-                std::integral_constant<unsigned, Nth_prime<n - 1>::value + shift>, 
-                impl::Nth_prime<n, shift + 2>
+                ::is_prime<Nth_prime<n - 1>::value + shift>::value,
+                std::integral_constant<unsigned, Nth_prime<n - 1>::value + shift>,
+				Nth_prime_shifted<n, shift + NextShift<Nth_prime<n - 1>::value + shift>::value>
             >{}
         >{};
 
-    template<>
-    struct Nth_prime<2>: std::integral_constant<unsigned, 3>{};
+    template<unsigned n>
+    struct Nth_prime : Nth_prime_shifted<n, NextShift<Nth_prime<n - 1>::value>::value>{};
 
-    template<>
-    struct Nth_prime<1>: std::integral_constant<unsigned, 2>{};
-
-    template<>
-    struct Nth_prime<0>;
+    template<> struct Nth_prime<3>: std::integral_constant<unsigned, 5>{};
+    template<> struct Nth_prime<2>: std::integral_constant<unsigned, 3>{};
+    template<> struct Nth_prime<1>: std::integral_constant<unsigned, 2>{};
+    template<> struct Nth_prime<0>;
 }
 
 template<unsigned n>
 struct Nth_prime : impl::Nth_prime<n>{};
 
 int main() {
-    std::cout << Nth_prime<10>::value << std::endl;
+    static_assert(Nth_prime<100>::value == 541);
+    static_assert(Nth_prime<10>::value == 29);
+    static_assert(Nth_prime<1>::value == 2);
+    static_assert(Nth_prime<2>::value == 3);
+    static_assert(Nth_prime<3>::value == 5);
     return 0;
 }
